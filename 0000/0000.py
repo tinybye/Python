f5b161a6d0510f6ba7f4eb96a17c6f59dbc66b17
tinybye <tinybye@gmail.com>
2016-01-10 11:18:27 +0800

    添加第一题注释以及知识点

--- 0000/0000.py
+++ 0000/0000.py
@@ -1,12 +1,19 @@
 from PIL import Image, ImageDraw, ImageFont
 
 def add_num(num, fill, font_name):
+    #打开图像文件
     im = Image.open('img.jpg')
+    #获取图像的尺寸
     xsize , ysize = im.size
+    #创建绘画对象
     draw = ImageDraw.Draw(im)
+    #获取右上角需要显示的数字
     text = str(num)
+    #创建Font对象
     font = ImageFont.truetype(font_name, xsize//3)
+    #添加文字
     draw.text((ysize//5 * 4, 0), text, fill, font)
+    #保存图像
     im.save("out.jpg")
 
 num = 4
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Python
 Python learning notes
 
-# 开始学习第0000题：
+# 第0000题：
 将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果。
 其实就是对图片的简单操作，首先是需要从PIL导入Image, ImageDraw, ImageFont。
+具体每一步代码都有注释解释。本题包含的知识点有以下几点：
+1、The ImageDraw Module 创建绘画对象，使用ImageDraw.Draw方法创建。使用了基本绘画操作的添加文字，方法为text((x,y),message,fill,font)，每个参数对应的是添加文字的坐标、文字、颜色、字体
+2、矢量字体支持 TrueType Font support 使用ImageFont.truetype(font,size)方法,每个参数对应的是字体的名称和字号。
+
+# 第0001题
